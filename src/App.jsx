ec49a3b87512c2ef5bcc669bb82769e0e2619f16
sejinbaek <sejin9767@gmail.com>
2025-05-10 23:02:17 +0900

    feat: 일기 기능을 위한 mockData 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/index.jsx";
+import { useReducer } from "react";
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기입니다",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기입니다",
+  },
+];
+
+const reducer = (state, action) => {
+  return state;
+};
 
 const App = () => {
+  const [data, dispatch] = useReducer(reducer, mockData);
   return (
     <>
       <RouterProvider router={router} />
