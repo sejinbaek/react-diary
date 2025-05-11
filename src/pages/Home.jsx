d9aa6827aa775793ff3b41d0582a9d3367b48ba7
sejinbaek <sejin9767@gmail.com>
2025-05-11 14:02:05 +0900

    feat: Header 날짜 이동 기능 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import { useState } from "react";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 
 const Home = () => {
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  // 이전 버튼 클릭 시 이전 달로 이동하는 함수
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+
+  // 다음 버튼 클릭 시 다음 달로 이동하는 함수
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
   return (
     <div>
       <Header
-        title={"2024년 2월"}
-        leftChild={<Button text={"<"} />}
-        rightChild={<Button text={">"} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
       />
       <DiaryList />
     </div>
